6cf548e949708f225b7e4162a91f6e3c431c41a1
Burkatt99 <tanya.bura.99@gmail.com>
2020-09-22 11:20:49 +0300

    Updates

---
+++ precache-manifest.cfa55ad76520ce1a24cd937e36f79136.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a0c6e559c274af348dd25a92ee163a",
+    "url": "/JetSolutions150/index.html"
+  },
+  {
+    "revision": "a169f72495c53c745200",
+    "url": "/JetSolutions150/static/css/main.3331e8c5.chunk.css"
+  },
+  {
+    "revision": "af46f6fd53be8fdfa7ae",
+    "url": "/JetSolutions150/static/js/2.58869b26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JetSolutions150/static/js/2.58869b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a169f72495c53c745200",
+    "url": "/JetSolutions150/static/js/main.2f0a597a.chunk.js"
+  },
+  {
+    "revision": "3532a3f04911bae4ae06",
+    "url": "/JetSolutions150/static/js/runtime-main.680b280c.js"
+  },
+  {
+    "revision": "fa3aaf3f7399cdc5be899828ea6e7582",
+    "url": "/JetSolutions150/static/media/animal.fa3aaf3f.jpg"
+  },
+  {
+    "revision": "d6ac6edc70de7c814f48872e57cf0c95",
+    "url": "/JetSolutions150/static/media/elephant.d6ac6edc.jpg"
+  },
+  {
+    "revision": "9aec3088e2e2e22ea5c1e41e52077afd",
+    "url": "/JetSolutions150/static/media/fox.9aec3088.jpg"
+  },
+  {
+    "revision": "5355e6aa0ab2665bb884ada056ff7a5f",
+    "url": "/JetSolutions150/static/media/horse.5355e6aa.jpg"
+  },
+  {
+    "revision": "dab9217067ea094eec0ed909d1114019",
+    "url": "/JetSolutions150/static/media/lazy.dab92170.jpg"
+  },
+  {
+    "revision": "ca2b91ddd6a576745b49c9a8501fe437",
+    "url": "/JetSolutions150/static/media/lion.ca2b91dd.jpg"
+  },
+  {
+    "revision": "9f1b96bdf0c7fb98b09d1180488fef45",
+    "url": "/JetSolutions150/static/media/logo.9f1b96bd.jpg"
+  },
+  {
+    "revision": "3d4e6ed00a7473b81304f62ae84f457f",
+    "url": "/JetSolutions150/static/media/panda.3d4e6ed0.jpg"
+  },
+  {
+    "revision": "98b09053e24ce48efc945a3830a3d2e2",
+    "url": "/JetSolutions150/static/media/pingvi.98b09053.jpg"
+  }
+]);
